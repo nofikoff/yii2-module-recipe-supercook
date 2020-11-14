caf04cdd32a6955cbd21f5113378efcf09c69539
nofikoffgit <ruslan.novikov@gmail.com>
2020-11-14 14:03:48 +0200

    модуль

--- Recipe.php
+++ Recipe.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace app\modules\recipe;
+namespace nofikoff\supercook;
 
 /**
  * recipe module definition class
@@ -10,7 +10,7 @@ class Recipe extends \yii\base\Module
     /**
      * {@inheritdoc}
      */
-    public $controllerNamespace = 'app\modules\recipe\controllers';
+    public $controllerNamespace = 'nofikoff\supercook\controllers';
 
     /**
      * {@inheritdoc}
